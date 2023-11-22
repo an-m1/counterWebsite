cbeb264902f685de238b74eebfc1840b4bdb054e
Ankit <ankitmodhera24@gmail.com>
2023-11-22 17:43:49 -0500

    Implementation of the 'Top' button

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ function navShrink() {
     x.className = "topnav";
   }
 }
-// Have the button change colour every time the cursor hovers and unhovers the button.
+
+//For the main button. Includes the colour change on hover and the value incrementation
 document.addEventListener("DOMContentLoaded", function () {
   var button = document.getElementById("dynamicColorButton");
   var navbar = document.getElementById("myTopnav");
@@ -61,3 +62,24 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   };
 });
+
+//For the 'go to top' button
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
