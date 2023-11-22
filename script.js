e5b5be81ebea16e6c103b2518860447eb7d85fb4
Ankit <ankitmodhera24@gmail.com>
2023-11-21 23:11:58 -0500

    Main Button Implementation

--- script.js
+++ script.js
@@ -7,3 +7,31 @@ function navShrink() {
       x.className = "topnav";
     }
   } 
+// Have the button change colour every time the cursor hovers and unhovers the button.
+  document.addEventListener('DOMContentLoaded', function () {
+    var button = document.getElementById('dynamicColorButton');
+    var navbar = document.getElementById('myTopnav');
+
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+    button.onmouseover = function() {
+      // navbar.style.display = 'none'; // Hide the navbar
+      navbar.classList.add('navbar-hidden');
+        // button.style.backgroundColor = getRandomColor();
+    };
+
+    button.onmouseleave = function() {
+      // navbar.style.display = 'block'; // Show the navbar again
+      navbar.classList.remove('navbar-hidden');
+        button.style.backgroundColor = getRandomColor();
+    };
+});
+
+
